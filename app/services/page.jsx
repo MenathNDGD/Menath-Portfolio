0bc3c50d94bd870bf060a25d1decc61ecd7b3bf9
MenathNDGD <nadungmenath1@gmail.com>
2025-02-18 20:01:14 +0530

    Refactor services component to import data from external servicesPageData module

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -3,58 +3,7 @@
 import { BsArrowDownRight } from "react-icons/bs";
 import Link from "next/link";
 import { motion } from "framer-motion";
-
-const services = [
-  {
-    num: "01",
-    title: "Web Development",
-    description:
-      "I build websites that serve as powerful marketing tools and bring memorable brand experiences.",
-    link: "web-development",
-  },
-  {
-    num: "02",
-    title: "Web Applications",
-    description:
-      "I develop web applications that help businesses automate their processes and increase efficiency.",
-    link: "web-applications",
-  },
-  {
-    num: "03",
-    title: "UI/UX Design",
-    description:
-      "I design user-friendly interfaces that are easy to navigate and provide a delightful experience.",
-    link: "ui-ux-design",
-  },
-  {
-    num: "04",
-    title: "Logo Design",
-    description:
-      "I create unique logos that help your brand stand out in the market and attract your target audience.",
-    link: "logo-design",
-  },
-  {
-    num: "05",
-    title: "Video Editing",
-    description:
-      "I edit videos that help businesses communicate their message clearly and effectively.",
-    link: "video-editing",
-  },
-  {
-    num: "06",
-    title: "Content Writing",
-    description:
-      "I write content that helps businesses attract and engage their target audience.",
-    link: "content-writing",
-  },
-  {
-    num: "07",
-    title: "Document Creation",
-    description:
-      "I create documents that help businesses communicate their message clearly and effectively.",
-    link: "document-creation",
-  },
-];
+import services from "@/data/servicesPageData";
 
 const Services = () => {
   return (
