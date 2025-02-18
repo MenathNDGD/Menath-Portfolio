a4a0b3f01722cf69a82237163394bc57a2e1359f
MenathNDGD <nadungmenath1@gmail.com>
2025-02-18 20:02:11 +0530

    Add resume page data module with personal information, experience, education, and skills sections

---
+++ data/resumePageData.js
@@ -0,0 +1,285 @@
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaJava,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+  FaPhp,
+} from "react-icons/fa";
+import {
+  SiTailwindcss,
+  SiNextdotjs,
+  SiTypescript,
+  SiMongodb,
+  SiMysql,
+  SiExpress,
+  SiCanva,
+  SiShadcnui,
+  SiFirebase,
+  SiLaravel,
+  SiAppwrite,
+  SiTrello,
+  SiVisualstudiocode,
+  SiC,
+  SiGithub,
+  SiVercel,
+} from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "I excel at creating intuitive, visually appealing web applications that enhance user experience, focusing on seamless interfaces and responsive full-stack solutions.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Menath Nadungoda",
+    },
+    {
+      fieldName: "Mobile",
+      fieldValue: "(+94) 76 549 3072",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "nadungmenath1@gmail.com",
+    },
+    {
+      fieldName: "Location",
+      fieldValue: "Colombo, Sri Lanka",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "03+ years",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Sri Lankan",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Sinhala",
+    },
+  ],
+};
+
+const experience = {
+  title: "My Experience",
+  description:
+    "With over three years of web development experience, I have utilized various technologies and frameworks to create robust, user-friendly applications that meet client needs.",
+  items: [
+    {
+      position: "Frontend Developer",
+      company: "Freelance",
+      date: "2024 - Present",
+    },
+    {
+      position: "Full Stack Developer",
+      company: "Smart Tech Solutions",
+      date: "2023 Jun - 2024",
+    },
+    {
+      position: "UI/Ux Designer",
+      company: "Freelance",
+      date: "2022 Sep - 2023 Jun",
+    },
+    {
+      position: "Trainee Intern",
+      company: "People's Bank",
+      date: "2022 Feb - 2022 Aug",
+    },
+    {
+      position: "Assistant Intern",
+      company: "Bank of Ceylon",
+      date: "2021 Jul - 2022 Mar",
+    },
+    {
+      position: "E-commerce Startup",
+      company: "Freelance Web Developer",
+      date: "2021 - 2022 Sep",
+    },
+    {
+      position: "UI/Ux Designer",
+      company: "Design Studio",
+      date: "2020 - 2021",
+    },
+    {
+      position: "Junior Video Editor",
+      company: "ModernViz Inc.",
+      date: "2020 Feb - 2020",
+    },
+    {
+      position: "Documentary Filmmaker",
+      company: "AloneMax Station",
+      date: "2019 - 2020 Feb",
+    },
+    {
+      position: "Content Creator",
+      company: "Freelance",
+      date: "2018 - 2019",
+    },
+  ],
+};
+
+const education = {
+  title: "My Education",
+  description:
+    "I am an undergraduate with a BSc(Hons.) in Computing & Information Systems (CIS) from the Sabaragamuwa University of Sri Lanka.",
+  items: [
+    {
+      degree: "BSc(Hons) in Computing & Information Systems",
+      university: "Sabaragamuwa University",
+      date: "2022 - Present",
+    },
+    {
+      degree: "Professional Skill Program on Agility",
+      university: "SLASSCOM, Colombo",
+      date: "20223 Oct",
+    },
+    {
+      degree: "Agile Project Management",
+      university: "Coursera",
+      date: "2023 Sep",
+    },
+    {
+      degree: "Foundations of Digital Marketing & E-commerce",
+      university: "Coursera",
+      date: "2023 Aug",
+    },
+    {
+      degree: "IT Security, Defence Against the Digital Dark",
+      university: "Coursera",
+      date: "2023 May",
+    },
+    {
+      degree: "The Fundamentals of Digital Marketing",
+      university: "Google Digital Garage",
+      date: "2023 Apr",
+    },
+    {
+      degree: "Diploma in Information Technology (DIT)",
+      university: "ICBT Campus, Colombo",
+      date: "2021 Apr - 2021 Oct",
+    },
+    {
+      degree: "General Certificate of Education (G.C.E) A/L",
+      university: "St. Aloysisus College, Galle",
+      date: "2018 - 2020",
+    },
+    {
+      degree: "General Certificate of Education (G.C.E) O/L",
+      university: "All Saints College, Galle",
+      date: "2017 - 2019",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "I have experience working with various technologies and frameworks, such as React, Node.js, and Firebase, enabling me to build robust and scalable applications.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "HTML 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "CSS 3",
+    },
+    {
+      icon: <SiShadcnui />,
+      name: "Shadcn UI",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      icon: <FaJava />,
+      name: "Java",
+    },
+    {
+      icon: <SiC />,
+      name: "C",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <SiTypescript />,
+      name: "TypeScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React JS",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next JS",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node JS",
+    },
+    {
+      icon: <SiExpress />,
+      name: "Express JS",
+    },
+    {
+      icon: <SiLaravel />,
+      name: "Laravel",
+    },
+    {
+      icon: <FaPhp />,
+      name: "PHP",
+    },
+    {
+      icon: <SiMongodb />,
+      name: "MongoDB",
+    },
+    {
+      icon: <SiFirebase />,
+      name: "Firebase",
+    },
+    {
+      icon: <SiMysql />,
+      name: "MySQL",
+    },
+    {
+      icon: <SiAppwrite />,
+      name: "Appwrite",
+    },
+    {
+      icon: <SiTrello />,
+      name: "Trello",
+    },
+    {
+      icon: <SiVisualstudiocode />,
+      name: "VS Code",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
+    {
+      icon: <SiCanva />,
+      name: "Canva",
+    },
+    {
+      icon: <SiGithub />,
+      name: "GitHub",
+    },
+    {
+      icon: <SiVercel />,
+      name: "Vercel",
+    },
+  ],
+};
+
+export { about, experience, education, skills };
