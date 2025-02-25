ae420801eeb6abcc2766d8349bb5bf96bb2c79f5
MenathNDGD <nadungmenath1@gmail.com>
2025-02-25 17:19:21 +0530

    Replace custom loading spinner with FaSpinner icon in contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -18,6 +18,7 @@ import {
 import { PhoneInput } from "@/components/PhoneInput";
 import MessageDialog from "@/components/MessageDialog";
 import emailjs from "emailjs-com";
+import { FaSpinner } from "react-icons/fa";
 import info from "@/data/contactPageData";
 
 const Contact = () => {
@@ -180,26 +181,7 @@ const Contact = () => {
               >
                 {isSending ? (
                   <div className="flex items-center">
-                    <svg
-                      className="w-5 h-5 mr-2 text-white animate-spin"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-                      ></path>
-                    </svg>
+                    <FaSpinner className="w-5 h-5 mr-2 text-white animate-spin" />
                     Sending...
                   </div>
                 ) : (
