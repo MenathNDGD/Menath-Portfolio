9530a70f5150c5faf18c279c4f3ab890a8741557
MenathNDGD <nadungmenath1@gmail.com>
2025-02-25 16:30:16 +0530

    Refactor Navbar component to use centralized navigation links data

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,42 +2,23 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-const links = [
-  {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "Services",
-    path: "/services",
-  },
-  {
-    name: "Resume",
-    path: "/resume",
-  },
-  {
-    name: "Work",
-    path: "/work",
-  },
-  {
-    name: "Contact",
-    path: "/contact",
-  },
-];
+import navLinks from "@/data/navbarData";
 
 const Navbar = () => {
   const pathname = usePathname();
 
   return (
     <nav className="flex gap-8">
-      {links.map((link, index) => {
+      {navLinks.map((link, index) => {
+        const isActive =
+          link.path === pathname || pathname.startsWith(`${link.path}/`);
+
         return (
           <Link
             href={link.path}
             key={index}
             className={`${
-              link.path === pathname && "text-accent border-b-2 border-accent"
+              isActive ? "text-accent border-b-2 border-accent" : ""
             } capitalize font-medium hover:text-accent transition-all`}
           >
             {link.name}
