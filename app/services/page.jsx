fa24d265a5ad1695c450cd7efd07884f536de31f
MenathNDGD <nadungmenath1@gmail.com>
2025-02-28 10:37:32 +0530

    Implement scrollable area for services section and adjust layout for better responsiveness

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -3,6 +3,7 @@
 import { BsArrowDownRight } from "react-icons/bs";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import services from "@/data/servicesPageData";
 
 const Services = () => {
@@ -15,31 +16,35 @@ const Services = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
           }}
-          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+          className="flex flex-col justify-center"
         >
-          {services.map((item, index) => (
-            <div
-              key={index}
-              className="flex flex-col justify-center flex-1 gap-6 group"
-            >
-              <div className="flex items-center justify-between w-full">
-                <div className="text-5xl font-extrabold text-transparent transition-all duration-500 text-outline group-hover:text-outline-hover">
-                  {item.num}
-                </div>
-                <Link
-                  href={`/services/${item.link}`}
-                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+          <ScrollArea className="h-[500px] px-1">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-[60px] p-4">
+              {services.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col justify-center flex-1 gap-6 group"
                 >
-                  <BsArrowDownRight className="text-3xl text-primary" />
-                </Link>
-              </div>
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
-                {item.title}
-              </h2>
-              <p className="text-white/60">{item.description}</p>
-              <div className="w-full border-b border-white/20"></div>
+                  <div className="flex items-center justify-between w-full">
+                    <div className="text-5xl font-extrabold text-transparent transition-all duration-500 text-outline group-hover:text-outline-hover">
+                      {item.num}
+                    </div>
+                    <Link
+                      href={`/services/${item.link}`}
+                      className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                    >
+                      <BsArrowDownRight className="text-3xl text-primary" />
+                    </Link>
+                  </div>
+                  <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                    {item.title}
+                  </h2>
+                  <p className="text-white/60">{item.description}</p>
+                  <div className="w-full border-b border-white/20"></div>
+                </div>
+              ))}
             </div>
-          ))}
+          </ScrollArea>
         </motion.div>
       </div>
     </section>
