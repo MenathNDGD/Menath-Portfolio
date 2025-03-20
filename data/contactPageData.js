7fd3e3e591f747b0248ef5c7ebcfb42bee20a36e
MenathNDGD <nadungmenath1@gmail.com>
2025-03-20 10:52:41 +0530

    Refactor contactPageData actions to use string identifiers for improved clarity

--- data/contactPageData.js
+++ data/contactPageData.js
@@ -11,36 +11,31 @@ const info = [
     icon: <FaPhoneAlt />,
     title: "Phone",
     description: "(+94) 76 549 3072",
-    action: () => (window.location.href = "tel:+94765493072"),
+    action: "call",
   },
   {
     icon: <FaEnvelope />,
     title: "Email",
     description: "nadungmenath1@gmail.com",
-    action: () => (window.location.href = "mailto:nadungmenath1@gmail.com"),
+    action: "email",
   },
   {
     icon: <FaMapMarkedAlt />,
     title: "Location",
     description: "Colombo, Sri Lanka",
-    action: () =>
-      window.open(
-        "https://www.google.com/maps/search/?api=1&query=Colombo,+Sri+Lanka",
-        "_blank"
-      ),
+    action: "location",
   },
   {
     icon: <FaLinkedin />,
     title: "LinkedIn",
     description: "Connect with me on LinkedIn",
-    action: () =>
-      window.open("https://www.linkedin.com/in/menath-ndgd/", "_blank"),
+    action: "linkedin",
   },
   {
     icon: <FaGithub />,
     title: "GitHub",
     description: "Check out my GitHub",
-    action: () => window.open("https://github.com/MenathNDGD", "_blank"),
+    action: "github",
   },
 ];
 
