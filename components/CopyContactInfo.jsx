c38ba02f309216505f3f76a02deba0acd2eb9944
MenathNDGD <nadungmenath1@gmail.com>
2025-03-21 08:31:22 +0530

    Add copy status indicators for email and phone in CopyContactInfo component

--- components/CopyContactInfo.jsx
+++ components/CopyContactInfo.jsx
@@ -1,17 +1,23 @@
 "use client";
 
 import { useState } from "react";
+import { Copy, ClipboardCheck } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 
 const CopyContactInfo = ({ email, phone }) => {
   const [emailCopyStatus, setEmailCopyStatus] = useState("Copy Email");
   const [phoneCopyStatus, setPhoneCopyStatus] = useState("Copy Phone");
+  const [emailCopied, setEmailCopied] = useState(false);
+  const [phoneCopied, setPhoneCopied] = useState(false);
 
   const handleCopyEmail = () => {
     navigator.clipboard.writeText(email).then(() => {
       setEmailCopyStatus("Copied!");
+      setEmailCopied(true);
       setTimeout(() => {
         setEmailCopyStatus("Copy Email");
+        setEmailCopied(false);
       }, 5000);
     });
   };
@@ -19,8 +25,10 @@ const CopyContactInfo = ({ email, phone }) => {
   const handleCopyPhone = () => {
     navigator.clipboard.writeText(phone).then(() => {
       setPhoneCopyStatus("Copied!");
+      setPhoneCopied(true);
       setTimeout(() => {
         setPhoneCopyStatus("Copy Phone");
+        setPhoneCopied(false);
       }, 5000);
     });
   };
@@ -32,6 +40,11 @@ const CopyContactInfo = ({ email, phone }) => {
           onClick={handleCopyEmail}
           className="px-3 py-1 mt-0 text-sm text-white bg-gray-900 rounded-lg hover:bg-gray-800"
         >
+          {!emailCopied ? (
+            <Copy className="inline-block w-4 h-4 mr-2" />
+          ) : (
+            <ClipboardCheck className="inline-block w-4 h-4 mr-2" />
+          )}
           {emailCopyStatus}
         </Button>
       )}
@@ -40,6 +53,11 @@ const CopyContactInfo = ({ email, phone }) => {
           onClick={handleCopyPhone}
           className="px-3 py-1 mt-0 text-sm text-white bg-gray-900 rounded-lg hover:bg-gray-800"
         >
+          {!phoneCopied ? (
+            <Copy className="inline-block w-4 h-4 mr-2" />
+          ) : (
+            <ClipboardCheck className="inline-block w-4 h-4 mr-2" />
+          )}
           {phoneCopyStatus}
         </Button>
       )}
