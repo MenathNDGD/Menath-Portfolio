873174680a3a13904d39422d634224ecfa034700
MenathNDGD <nadungmenath1@gmail.com>
2025-03-21 08:32:31 +0530

    Reorganize import statements in Header component for improved readability

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
+
 import { Button } from "./ui/button";
 
-// INFO: Importing the components
 import Navbar from "./Navbar";
 import MobileNavbar from "./MobileNavbar";
 
