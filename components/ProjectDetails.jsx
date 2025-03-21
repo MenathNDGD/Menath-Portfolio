7caf2a8b0fd790da4c65309114a215fde0423bab
MenathNDGD <nadungmenath1@gmail.com>
2025-03-21 08:34:21 +0530

    Reorganize import statements in ProjectDetails component for improved readability

--- components/ProjectDetails.jsx
+++ components/ProjectDetails.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
 import {
   Carousel,
   CarouselContent,
