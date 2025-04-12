de0ac6c71886393ded5d333ba3d40cfd1816d78b
MenathNDGD <nadungmenath1@gmail.com>
2025-04-12 11:28:17 +0530

    Add assets module to manage images and documents for the portfolio

---
+++ utils/assets.js
@@ -0,0 +1,30 @@
+export const images = {
+  logo: "/assets/logo.png",
+  hero: "/assets/hero.png",
+};
+
+export const serviceImages = {
+  web_dev: "/assets/service/web-development.png",
+  web_app: "/assets/service/web-applications.png",
+  ui_ux: "/assets/service/ui-ux-design.png",
+  logo_design: "/assets/service/logo-design.png",
+  video_editing: "/assets/service/video-editing.png",
+  content_writing: "/assets/service/content-writing.png",
+  doc_creation: "/assets/service/document-creation.png",
+  custom_service: "/assets/service/custom-service.png",
+};
+
+export const projectImages = {
+  project1: "/assets/work/car-rent-4u.png",
+  project2: "/assets/work/edu-quest.png",
+  project3: "/assets/work/care-sync.png",
+  project4: "/assets/work/e-comm-platform.png",
+  project5: "/assets/work/eco-bin.png",
+  project6: "/assets/work/trendify.png",
+  project7: "/assets/work/tasty-trails.png",
+  project8: "/assets/work/next-bank.png",
+};
+
+export const documents = {
+  resume: "/assets/resume.pdf",
+};
